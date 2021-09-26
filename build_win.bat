9050de86eff4f6a9782a2e1c8b885fef7f1cf47d
Davide Romanini <davide.romanini@gmail.com>
2021-09-26 19:52:46 +0200

    fix: try to use x64 vc command prompt

--- build_win.bat
+++ build_win.bat
@@ -1,2 +1,2 @@
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 %*
